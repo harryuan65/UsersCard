eabf67890e76de8ecab10d601979b59d6e131024
harryuan65 <harryuan.65@gmail.com>
2023-02-12 22:18:41 +0800

    feat: implement rotating imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Female1 from './images/female1.jpeg';
+import Female2 from './images/female2.jpeg';
+import Female3 from './images/female3.jpeg';
+import Male1 from './images/male1.jpeg';
+import Male2 from './images/male2.jpeg';
+import Male3 from './images/male3.jpeg';
+import styles from './App.module.css';
 
 function App() {
+  const usersImages = [Female1, Female2, Female3, Male1, Male2, Male3];
+  const itemCountStyle = {
+    '--item-count': usersImages.length,
+  } as React.CSSProperties;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.wrapper} style={itemCountStyle}>
+        {usersImages.map((src, i) => {
+          const style = { '--i': i } as React.CSSProperties;
+          return (
+            <img
+              alt={`img${i}`}
+              src={src}
+              className={styles.userImage}
+              style={style}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
