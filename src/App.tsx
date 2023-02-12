bf3c42bf0075960e653b002d92fb0f0a50198abe
harryuan65 <harryuan.65@gmail.com>
2023-02-13 00:19:48 +0800

    feat: finish the rest 101

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,24 @@ import Female2 from './images/female2.jpeg';
 import Female3 from './images/female3.jpeg';
 import Male1 from './images/male1.jpeg';
 import Male2 from './images/male2.jpeg';
+import styles from './App.module.css';
 
 function App() {
   const usersImages = [Female1, Female2, Female3, Male1, Male2];
 
   return (
     <div className="App">
-      <CyclingImages srcs={usersImages} />
+      <div className={styles.card}>
+        <CyclingImages srcs={usersImages} />
+        <div className={styles.messageContainer}>
+          <h2 className={styles.title}>Looking for some inspiration?</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vade Post
+            me Satana! Exaudi Domine Patri.
+          </p>
+        </div>
+        <button className={styles.button}>SEE FEATURED CREATIVES</button>
+      </div>
     </div>
   );
 }
