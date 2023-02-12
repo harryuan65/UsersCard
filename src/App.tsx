39520755272e5f61bc16736a8fd6316cf9b8884d
harryuan65 <harryuan.65@gmail.com>
2023-02-12 23:27:45 +0800

    chore: reduce to 5 images

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import Female2 from './images/female2.jpeg';
 import Female3 from './images/female3.jpeg';
 import Male1 from './images/male1.jpeg';
 import Male2 from './images/male2.jpeg';
-import Male3 from './images/male3.jpeg';
 import styles from './App.module.css';
 
 function App() {
-  const allImages = [Female1, Female2, Female3, Male1, Male2, Male3, Male1];
-  const usersImages = allImages.slice(0, 7); // experimenting with length
+  const usersImages = [Female1, Female2, Female3, Male1, Male2];
   const itemCountStyle = {
     '--item-count': usersImages.length,
     '--rotate-apply-adjustment': usersImages.length % 2 === 0 ? 0 : 1,
