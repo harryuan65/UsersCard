bc48a483a2ee6c84b34bcbacd88c1c19d0864158
harryuan65 <harryuan.65@gmail.com>
2023-02-12 23:30:29 +0800

    refactor: cycling images into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
+import CyclingImages from './components/CyclingImages';
 import Female1 from './images/female1.jpeg';
 import Female2 from './images/female2.jpeg';
 import Female3 from './images/female3.jpeg';
 import Male1 from './images/male1.jpeg';
 import Male2 from './images/male2.jpeg';
-import styles from './App.module.css';
 
 function App() {
   const usersImages = [Female1, Female2, Female3, Male1, Male2];
-  const itemCountStyle = {
-    '--item-count': usersImages.length,
-    '--rotate-apply-adjustment': usersImages.length % 2 === 0 ? 0 : 1,
-  } as React.CSSProperties;
 
   return (
     <div className="App">
-      <div className={styles.wrapper} style={itemCountStyle}>
-        {usersImages.map((src, i) => {
-          const style = { '--i': i } as React.CSSProperties;
-          return (
-            <img
-              alt={`img${i}`}
-              src={src}
-              className={styles.userImage}
-              style={style}
-            />
-          );
-        })}
-      </div>
+      <CyclingImages srcs={usersImages} />
     </div>
   );
 }
---
+++ src/components/CyclingImages/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+interface CyclingImagesProps {
+  srcs: string[];
+}
+
+const CyclingImages = ({ srcs }: CyclingImagesProps) => {
+  const itemCountStyle = {
+    '--item-count': srcs.length,
+    '--rotate-apply-adjustment': srcs.length % 2 === 0 ? 0 : 1,
+  } as React.CSSProperties;
+  return (
+    <div className={styles.wrapper} style={itemCountStyle}>
+      {srcs.map((src, i) => {
+        const style = { '--i': i } as React.CSSProperties;
+        return (
+          <img
+            alt={`img${i}`}
+            src={src}
+            className={styles.userImage}
+            style={style}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CyclingImages;
